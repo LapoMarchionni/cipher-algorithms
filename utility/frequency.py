89eb69ef468bb553743119a87afc8a796da3eddc
Lapo Marchionni <marchionni.lapo@gmail.com>
2018-04-12 10:43:14 +0200

    Frequency shift

--- utility/frequency.py
+++ utility/frequency.py
@@ -1,7 +1,7 @@
 from collections import Counter
 from moby_dick import CHAPTER_ONE
 
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import numpy as np
 import string
 import re
@@ -38,15 +38,58 @@ def letters_frequency(text):
     plt.show()
 
 
-def empiric_distrobution(q, text):
-    """ TODO """
-    assert q >= 2, ("Choose a q >= 2")
-    q_grams = [text[n:n+2] for n in range(0, len(text), q)]
-    counter = Counter(q_grams)
+FP = {
+    'A': 0.08167,
+    'B': 0.01492,
+    'C': 0.02782,
+    'D': 0.04253,
+    'E': 0.12702,
+    'F': 0.02228,
+    'G': 0.02015,
+    'H': 0.06094,
+    'I': 0.06966,
+    'J': 0.00153,
+    'K': 0.00772,
+    'L': 0.04025,
+    'M': 0.02406,
+    'N': 0.06749,
+    'O': 0.07507,
+    'P': 0.01929,
+    'Q': 0.00095,
+    'R': 0.05987,
+    'S': 0.06327,
+    'T': 0.09056,
+    'U': 0.02758,
+    'V': 0.00978,
+    'W': 0.0236,
+    'X': 0.0015,
+    'Y': 0.01974,
+    'Z': 0.00074
+}
 
 
-def coincidence_and_entropy(q, text):
-    """ TODO """
-    assert q >= 2, ("Choose a q >= 2")
-    q_grams = [text[n:n+2] for n in range(0, len(text), q)]
-    counter = Counter(q_grams)
+def shift_frequency(row):
+    """Returns the max Mg value of a single row by comparing every
+    possible letter.
+    """
+    fp = list(FP.values())
+    row_count, row_len = Counter(row), len(row)
+    max_value, max_g = 0, 0
+    row = list(set(row))
+    row_d = {l: row_count[l] for l in row}
+    alpha = string.ascii_uppercase
+    for l in string.ascii_uppercase:
+        if l not in row_d:
+            row_d[l] = 0
+    for g in range(26):
+        mg = []
+        for j in range(26):
+            letter = alpha[(g+j) % 26]
+            fg = row_count[letter]/row_len
+            pj = fp[j]
+            mg.append(fg*pj)
+        value_mg = sum(mg)
+        if value_mg > max_value:
+            max_value = value_mg
+            max_g = alpha[g]
+    return max_g, max_value
